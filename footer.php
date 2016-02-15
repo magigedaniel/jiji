fe6f169c4fb94ad963bbccf676241fb5ce56d388
Magige Daniel <magigedaniel@gmail.com>
2016-02-15 13:25:24 +0300

    All updates

--- footer.php
+++ footer.php
@@ -1,3 +1,97 @@
+<section id="footer">
+    <div class="road"></div>
+    <div id="carWrap"><div class="car"></div></div>
+
+    <section class="container">
+        <div class="row">
+            <aside class="col-md-2 col-sm-12">
+                <div class="colInner">
+                    <h4>Discover</h4>
+                    <h5><a href="about">About us</a></h5>
+                    <h5><a href="features.1">Features</a></h5>
+                    <h5><a href="pricing/index.html">Pricing</a></h5>
+                    <!--                    <h5><a href="/case-studies">Case Study</a></h5>-->
+                    <h5><a href="http://blog.JIJICOM.com">Blog</a></h5>
+                    <h5><a href="http://api.JIJICOM.com/apidoc">API</a></h5>
+                </div>
+            </aside>
+
+            <aside class="col-md-2 col-sm-12">
+                <div class="colInner">
+                    <h4>Industries</h4>
+                    <h5><a href="startups.1">Startups</a></h5>
+                    <h5><a href="sms-in-education">Education</a></h5>
+                    <h5><a href="sms-for-alerts#clock">Stock Market</a></h5>
+                    <h5><a href="internal-communication/index.html">Internal Communication</a></h5>
+                    <h5><a href="sms-for-developers">Developers</a></h5>
+                </div>
+            </aside>
+
+            <aside class="col-md-2 col-sm-12">
+                <div class="colInner">
+                    <h4>Services</h4>
+                    <h5><a href="admin-panel/index.html">Admin Panel on rent</a></h5>
+                    <h5><a href="sms-reseller/index.html">SMS Reseller</a></h5>
+                    <h5><a href="longcode">Long Code</a></h5>
+                    <!--<h5><a href="/voice-sms">Voice SMS</a></h5>-->
+                    <h5><a href="transactional-sms">Transactional SMS</a></h5>
+                    <h5><a href="http://sendotp.JIJICOM.com/">SendOTP</a></h5>
+                </div>
+            </aside>
+
+            <aside class="col-md-2 col-sm-12">
+                <div class="colInner">
+                    <h4>Tools &amp; Add-ons</h4>
+                    <h5><a href="sms-for-spreadsheets">Excel plugin</a></h5>
+                    <h5><a href="sms-for-spreadsheets">Google spreadsheet</a></h5>
+                    <h5><a href="http://www.magentocommerce.com/magento-connect/JIJICOM-messaging-extension.html">Magento plugin</a></h5>
+                    <h5><a href="cc2sms/index.html">CC2SMS</a></h5>
+                    <h5><a href="http://smsaddons.com/" target="_blank">More Add-ons</a></h5>
+                    <br/><br/>
+                    <h5>Download Apps</h5>
+                    <ul class="dload-app clearfix sc">
+                        <li id="apple"><a target="_blank" href="https://itunes.apple.com/us/app" class="app apple"></a></li>
+                        <li id="android"><a target="_blank" href="https://play.google.com/store/apps" class="app android"></a></li>
+                        <li id="windows"><a target="_blank" href="http://www.windowsphone.com/en-in/store/app" class="app windows"></a></li>
+                    </ul>
+                </div>
+            </aside>
+
+            <aside class="col-md-2 col-sm-12">
+                <div class="colInner">
+                    <h4>Stay Connected</h4>
+                    <h5 ><a href="contact">Contact us</a></h5>
+                    <h5><a target="_blank" href="http://walkover.in/currentopening.php">We are hiring!</a></h5>
+                    <br/><br/>
+                    <h5>Find us on</h5>
+                    <ul class="social clearfix sc">
+                        <li id="fb"><a target="_blank" rel="nofollow" href="https://www.facebook.com/"><i class="ic-32 fb"></i></a></li>
+                        <li id="gp"><a target="_blank" rel="nofollow" href="https://plus.google.com/"><i class="ic-32 gp"></i></a></li>
+                        <li id="tw"><a target="_blank" rel="nofollow" href="https://twitter.com/"><i class="ic-32 tw"></i></a></li>
+                        <li id="in" class="last"><a target="_blank" rel="nofollow" href="http://www.linkedin.com/"><i class="ic-32 in"></i></a></li>
+                    </ul>
+                </div>
+            </aside>
+        </div>
+    </section><!--/end of first row-->
+
+
+    <section id="seosec">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <div class="seosec">
+                        <article><strong>JijiCom media</strong><br> You are doing it, your competitor is doing it and every big or small company is doing it. Sending JijiCom media is like speaking to all your clients in one go. It’s a fast paced world. Time is less and things to be done are galore. Everyone seeks shortcuts or a fast and easy way to get things done. So much that even a form of communication has been witnessing the change to this for quite some time now.</article>
+                        <article><strong>Experience the change with JIJICOM</strong><br> No wonder that the evolution of conveying messages has narrowed down from walking miles, flying pigeons, writing letters to just an SMS. Power of JijiCom media- The Short Messaging Service (SMS) has served more purposes than it initially intended to. Until recently, it has become a powerful tool for mobile marketing used by industries of almost every sector. Since, SMS is comparatively a faster and an easier mode of communication, companies have heeded in its power to send out JijiCom media for various reasons concerning them. JIJICOM– Possibly the Best JijiCom media Solutions Company Catering to these concerns, we thought of taking this process of communication altogether to a new level and that’s how we gave birth to JIJICOM.</article>
+                        <article><strong>What JIJICOM has to offer you?</strong><br> JIJICOM is a smart messaging solution that disseminates JijiCom media via its SMS gateway in a blink. Its robust infrastructure allows you to generate instant reports and send up to 1 Lakh SMS with just one click. Our various add-ons like JijiCom media excel plugin, JijiCom media mobile app, CC2SMS, a rewarding reseller program etc will give you endless opportunities to make your business communication easy, smooth, productive and easy on pocket. With every new step we try unlocking new levels of possibilities and unfold new chapters to give novel dimensions to messaging and enhance user experience.</article>
+                        <article><strong>Grow with us!</strong><br>We have been in the JijiCom media industry from the past seven years. We have a proven track record of delivering JijiCom media in 90+ countries across diverse business sectors and our myriad of happy customers sends out the message of our efficacy.</article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</section><!--/end of foooter-->
 <footer>
     <div class="container">
         <div class="row">
